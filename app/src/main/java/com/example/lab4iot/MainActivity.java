c09e4e743b086931049e97d39570f14f5755e563
uwu <si@pucp.edu.pe>
2022-11-04 15:00:26 -0500

    avcance login

--- app/src/main/java/com/example/lab4iot/MainActivity.java
+++ app/src/main/java/com/example/lab4iot/MainActivity.java
@@ -1,10 +1,32 @@
 package com.example.lab4iot;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +35,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void prueba(View view){
-        Intent intent = new Intent(MainActivity.this, Admin.class);
-        startActivity(intent);
+
+    public void redirigirAdmin(View view) {
+
+        Intent fbIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                .build();
+        activityResultLauncher.launch(fbIntent);
+    }
+
+
+    public void redirigirUser(View view) {
+        //startActivity(new Intent(MainActivity.this,));
+    }
+
+    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new FirebaseAuthUIActivityResultContract(), result -> {
+        onSignInOnResult(result);
+    });
+
+    private void onSignInOnResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse idpResponse = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            Log.d("msg-fb", currentUser.getUid());
+
+        } else {
+            Log.d("msg-fb", "error al loguearse");
+        }
     }
 }
