be82fc53be791931b39e1e596bee6da95ca26032
KatherineObregon <a20181823@pucp.edu.pe>
2022-11-04 09:16:52 -0500

    admin guardar

--- app/src/main/java/com/example/lab4iot/Admin.java
+++ app/src/main/java/com/example/lab4iot/Admin.java
@@ -4,16 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.lab4iot.beans.Jugador;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Admin extends AppCompatActivity {
-
+    FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
     }
 
     public void guardarInformacion(View view){
 
+    }
+
+    public void guardarInfoDeportivo(View view) {
+        DatabaseReference ref = firebaseDatabase.getReference();
+        DatabaseReference refEncuentroDeportivo = ref.child("encuentro_deportivo");
+
+        EditText editTextEquipo = findViewById(R.id.inputEquipo);
+        EditText editTextNombreJugador = findViewById(R.id.inputNombreJugador);
+        EditText editTextApellidoJugador = findViewById(R.id.inputApellidoJugador);
+        EditText editTexHito = findViewById(R.id.inputHito);
+        String equipo = editTextEquipo.getText().toString();
+
+        DatabaseReference refEquipo = refEncuentroDeportivo.child(equipo);
+
+        //HashMap<String,String> hashMap = new HashMap<>();
+        //hashMap.put("nombre",editTextNombre.getText().toString());
+        //hashMap.put("apellido",editTextApellido.getText().toString());
+        //ref.setValue(hashMap);
+        Jugador jugador = new Jugador();
+        jugador.setNombre(editTextNombreJugador.getText().toString());
+        jugador.setApellido(editTextApellidoJugador.getText().toString());
+        jugador.setHito(editTexHito.getText().toString());
+
+//        refEquipo.setValue(jugador).addOnSuccessListener(unused -> {
+//            //Snackbar.make(findViewById(R.id.MainActivityLayout),"Usuario registrado correctamente",2000).show();
+//            Toast.makeText(MainActivity.this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+//        });
     }
 }
---
+++ app/src/main/java/com/example/lab4iot/beans/Jugador.java
@@ -0,0 +1,33 @@
+package com.example.lab4iot.beans;
+
+import java.util.PrimitiveIterator;
+
+public class Jugador {
+    private String nombre;
+    private String apellido;
+    private String hito;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getHito() {
+        return hito;
+    }
+
+    public void setHito(String hito) {
+        this.hito = hito;
+    }
+}
