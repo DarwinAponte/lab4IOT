30535ff7a8910d46fc388628e467284a12319fc6
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2022-11-06 16:18:51 -0500

    algunos ajustes

--- app/src/main/java/com/example/lab4iot/User_hitos.java
+++ app/src/main/java/com/example/lab4iot/User_hitos.java
@@ -2,9 +2,11 @@ package com.example.lab4iot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.lab4iot.beans.Jugador;
 import com.google.firebase.database.DataSnapshot;
@@ -13,68 +15,54 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class User_hitos extends AppCompatActivity {
 
+    RecyclerView recyclerView1, recyclerView2;
+    ArrayList<Jugador> list1,list2;
+    DatabaseReference databaseReference1,databaseReference2;
+    MyAdapter adapter1, adapter2;
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    ArrayList<Jugador> list;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(User_hitos.this, UserActivity.class));
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_hitos);
-
-        TextView txtPUCP1=findViewById(R.id.txtPUCP1);
-        TextView txtPUCP2=findViewById(R.id.txtPUCP2);
-        TextView txtPUCP3=findViewById(R.id.txtPUCP3);
-        TextView txtPUCP4=findViewById(R.id.txtPUCP4);
-        TextView txtPUCP5=findViewById(R.id.txtPUCP5);
-        TextView txtPUCP6=findViewById(R.id.txtPUCP6);
-        TextView txtPUCP7=findViewById(R.id.txtPUCP7);
-        TextView txtPUCP8=findViewById(R.id.txtPUCP8);
-        TextView txtPUCP9=findViewById(R.id.txtPUCP9);
-        TextView txtPUCP10=findViewById(R.id.txtPUCP10);
-        TextView txtPUCP11=findViewById(R.id.txtPUCP11);
-
-        TextView txtUPC1=findViewById(R.id.txtUPC1);
-        TextView txtUPC2=findViewById(R.id.txtUPC2);
-        TextView txtUPC3=findViewById(R.id.txtUPC3);
-        TextView txtUPC4=findViewById(R.id.txtUPC4);
-        TextView txtUPC5=findViewById(R.id.txtUPC5);
-        TextView txtUPC6=findViewById(R.id.txtUPC6);
-        TextView txtUPC7=findViewById(R.id.txtUPC7);
-        TextView txtUPC8=findViewById(R.id.txtUPC8);
-        TextView txtUPC9=findViewById(R.id.txtUPC9);
-        TextView txtUPC10=findViewById(R.id.txtUPC10);
-        TextView txtUPC11=findViewById(R.id.txtUPC11);
-
+        setContentView(R.layout.activity_user_alineacion);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference= firebaseDatabase.getReference("encuentro_deportivo").child("PUCP TEAM");
-        databaseReference.addValueEventListener(new ValueEventListener() {
+
+        //PARA EQUIPO B
+        recyclerView1=findViewById(R.id.recycleView1);
+        databaseReference1= firebaseDatabase.getReference("encuentro_deportivo").child("UPC SPORT");
+        list1=new ArrayList<>();
+        recyclerView1.setLayoutManager(new LinearLayoutManager(User_hitos.this));
+        adapter1 = new MyAdapter(this,list1);
+        recyclerView1.setAdapter(adapter1);
+
+        //PARA EQUIPO A
+        recyclerView2=findViewById(R.id.recycleView2);
+        databaseReference2= firebaseDatabase.getReference("encuentro_deportivo").child("PUCP SPORT");
+        list2=new ArrayList<>();
+        recyclerView2.setLayoutManager(new LinearLayoutManager(User_hitos.this));
+        adapter2 = new MyAdapter(this,list2);
+        recyclerView2.setAdapter(adapter2);
+
+        databaseReference1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot dataSnapshot: snapshot.getChildren()){
                     Jugador jugador=dataSnapshot.getValue(Jugador.class);
-//                    list.add(jugador);
-                    System.out.println(jugador.getNombre());
+                    list1.add(jugador);
                 }
-
-//                String player="txtPUCP";
-//                boolean n=true;
-//                for(Jugador jugador: list){
-//                    if(n){
-//                        txtPUCP1.setText(jugador.getNombre());
-//                    }else {n=false;}
-//
-//                }
-
-
+                adapter1.notifyDataSetChanged();
             }
 
             @Override
@@ -82,5 +70,26 @@ public class User_hitos extends AppCompatActivity {
 
             }
         });
+
+        databaseReference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Jugador jugador=dataSnapshot.getValue(Jugador.class);
+                    list2.add(jugador);
+                }
+                adapter2.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
     }
 }
